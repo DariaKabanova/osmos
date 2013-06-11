cb534d257046bd3b8b6f06baa95640e3e319c3c5
Daria Kabanova <d.s.kabanova@gmail.com>
2013-06-11 18:31:47 +0400

    start

---
+++ Osmos/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Osmos
+//
+//  Created by Дарья Кабанова on 11.06.13.
+//  Copyright (c) 2013 Дарья Кабанова. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
