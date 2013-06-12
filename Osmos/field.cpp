ce9acc9fba1e7e78d35a7926f8d38c2638582f60
Daria Kabanova <d.s.kabanova@gmail.com>
2013-06-12 15:26:53 +0400

    add class Field

---
+++ Osmos/field.cpp
@@ -0,0 +1,14 @@
+//
+//  field.cpp
+//  Osmos
+//
+//  Created by Дарья Кабанова on 12.06.13.
+//  Copyright (c) 2013 Дарья Кабанова. All rights reserved.
+//
+
+#include "field.h"
+
+Field::Field() {
+    Circle circle;//=new Circle();
+    Field::v.push_back(circle);
+}
---
+++ Osmos/field.h
@@ -0,0 +1,29 @@
+//
+//  field.h
+//  Osmos
+//
+//  Created by Дарья Кабанова on 12.06.13.
+//  Copyright (c) 2013 Дарья Кабанова. All rights reserved.
+//
+
+#ifndef __Osmos__field__
+#define __Osmos__field__
+
+#include <iostream>
+
+#include "circle.h"
+#include <vector>
+
+using namespace std;
+
+
+class Field {
+private: vector<Circle> v;
+    
+public: Field();
+    
+    
+};
+
+
+#endif /* defined(__Osmos__field__) */
